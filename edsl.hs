2d8b7c7855736ecdf318d7a885a251cb1fe79cec
damodharanj <damojay@gmail.com>
2018-08-12 23:50:24 +0530

    List examples

--- edsl.hs
+++ edsl.hs
@@ -1,4 +1,3 @@
-
 plus a b = a + b
 
 minus a b = a - b
@@ -23,7 +22,7 @@ instance Show Can where
     show (Illa) = "Onnum illa :("
 
 instance Show Prediction where
-    show (Dharan x y z) = "Dei inga varudhe da!"
+    show (Dharan x y z) = "Dei inga varudhe da!" ++ show (x + y + z)
 
 data Expr
     = Val Int
@@ -48,24 +47,24 @@ eval expr = case expr of
         else
             pure (r1 `div` r2)
 
--- maybeAdd :: Maybe Int -> Maybe Int -> Maybe Int
-
 z = (Dharan 10 20 40)
 
 main = do
     putStrLn (show (x))
     putStrLn (show (y))
+    putStrLn (show (z))
     putStrLn (show (Damo 4))
     putStrLn (show Illa)
     putStrLn (show (eval (Mul (Add (Val 1) (Val 2)) (Div (Val 8) (Val 2)))))
     putStrLn (show (foldl (*) 2 [1,2,3]))
     putStrLn (show (fmap (*2) [1,2,3]))
     putStrLn (show (fmap (*2) (Just 5)))
-    putStrLn (show ((*2) <$> (Just 10))) -- functor!!!
-    putStrLn (show (Just (*3) <*> (Just 10))) -- applicative!!!
-    putStrLn (show ((Just 3) >>= (\x -> Just (2 * x)))) -- monad!!!!
-    putStrLn (show ([1,2,3] >>= (\x -> [2 * x])))
-    -- putStrLn (show z)
+    putStrLn (show ((*2) <$> (Just 10))) -- maybe functor!!!
+    putStrLn (show ((*2) <$> [1,2,3,4])) -- list functor!!!
+    putStrLn (show ((*) <$> Just (3) <*> (Just 10))) -- maybe applicative!!!
+    putStrLn (show ([(*2), (+5)] <*> [1,2,4])) -- list applicative
+    putStrLn (show ((Just 3) >>= (\x -> Just (2 * x)))) -- maybe monad!!!!
+    putStrLn (show ([1,2,3] >>= (\x -> [2 * x]))) -- list monad!!!!
 
 -- Functor example
 
